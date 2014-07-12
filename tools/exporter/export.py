90da6c96e37c873383865bf73faea8741f1b9c8d
0xc0170 <c0170@rocketmail.com>
2014-07-12 20:13:09 +0100

    [Exporters] export.py - remove commented code
    (cherry picked from commit fff525af51dd7d8fe3e2a3cb29322eb29bfb01fd)

--- tools/exporter/export.py
+++ tools/exporter/export.py
@@ -5,33 +5,10 @@ import os
 from yaml_parser import parse_yaml
 from uvision4 import Uvision4
 
-#from workspace_tools.bootloader import BOOTLOADER_TARGETS
-
-# class Loader(yaml.Loader):
-
-#     def __init__(self, stream):
-
-#         self._root = os.path.split(stream.name)[0]
-
-#         super(Loader, self).__init__(stream)
-
-#     def include(self, node):
-
-#         filename = os.path.join(self._root, self.construct_scalar(node))
-
-#         with open(filename, 'r') as f:
-#             return yaml.load(f, Loader)
-
-#Loader.add_constructor('!include', Loader.include)
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     # Parse Options
     parser = OptionParser()
-
-
-#    parser.add_option("-b", "--bootloader", metavar="MCU", default='K20',
-#        help="bootloader project build (%s)" % ', '.join(BOOTLOADER_TARGETS))
 
     (options, args) = parser.parse_args()
 
@@ -50,10 +27,6 @@ if __name__ == '__main__':
     config.update(yaml.load(f_ubs))
     config.update(yaml.load(f_rtos))
     config.update(yaml.load(f_flash))
-
-    #logging.info(config)
-    # for data in config:
-    #print config
 
     f_boot.close()
     f_uvision.close()
