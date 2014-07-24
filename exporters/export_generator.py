e0b8efd3c8a29d58d47553ca2904f1f8ee661c0c
sg- <sam.w.grove@gmail.com>
2014-07-24 17:14:12 -0500

    Update directory of project files
    (cherry picked from commit 4087f6d5bfff19c28d92c835a72d5a4f3b2ca001)
    
    Conflicts:
    	interface/mdk/k20dx128/k20dx128_interface.uvproj

--- exporters/export_generator.py
+++ exporters/export_generator.py
@@ -18,10 +18,13 @@ class Exporter():
         template = Template(template_text)
         target_text = template.render(data)
 
-        project_file_loc = 'project_files' + '/'+ data['name'] + '_' + ide
+        project_file_loc = ide + '_' + 'project_files\\'
         if not os.path.exists(project_file_loc):
             os.makedirs(project_file_loc)
-        target_path = join(project_file_loc, target_file)
+        project_dir = project_file_loc + data['name']
+        if not os.path.exists(project_dir):
+            os.makedirs(project_dir)
+        target_path = join(project_dir, target_file)
         logging.debug("Generating: %s" % target_path)
         open(target_path, "w").write(target_text)
 
