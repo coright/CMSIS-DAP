a8cf6ae0308a2d7c1b86e71ea6e25533245fed17
sg- <sam.w.grove@gmail.com>
2014-09-16 15:35:20 -0500

    [refactor] Updating openlink example project and components locations

--- source/common/firmware_cfg.h
+++ source/common/firmware_cfg.h
@@ -49,6 +49,7 @@ extern fw_cfg_t const app;
 #ifdef __cplusplus
 }
 #endif
+
 // TARGET_MK20DX,  K20DX128,  CPU_MK20DX128VFM5,  __RTX,  BOOTLOADER, 
 #if defined(BOOTLOADER) && defined(K20DX128)
 #include "MK20D5.h"
@@ -56,6 +57,11 @@ extern fw_cfg_t const app;
 #define FLASH_SIZE_KB   (128-32)
 #define INITIAL_SP      (*(uint32_t *)(APP_START_ADR))
 #define RESET_HANDLER   (*(uint32_t *)(APP_START_ADR + 4))
+#endif
+
+// TARGET_MK20D5,  __RTX,  OFFSET_VTABLE_32K,  DBG_KL25Z,  BOARD_FRDM_KL25Z,  INTERFACE_GEN_32KHZ,  OPENSDA_BOOTLOADER, 
+#if defined (DBG_KL25Z)
+#define FLASH_SIZE_KB   128
 #endif
 
 /**
